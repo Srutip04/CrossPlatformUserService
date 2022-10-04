500cd5eba660640525b491d75dded152721dc960
Srutip04 <subuhishruti75@gmail.com>
2022-10-04 20:58:53 +0530

    error handlers

--- server/middlewares/http-error.js
+++ server/middlewares/http-error.js
@@ -1,8 +1,16 @@
-class HttpError extends Error{
-    constructor(message,errorCode){
-        super(message);
-        this.code = errorCode;
-    }
-}
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
 
-module.exports = HttpError;
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+module.exports = { notFound, errorHandler };
