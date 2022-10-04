f74708250ece8d003953b26db43d9df1cefa9f6d
Srutip04 <subuhishruti75@gmail.com>
2022-10-04 15:14:42 +0530

    password encryption with bcrypt

--- server/models/users-model.js
+++ server/models/users-model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
 const uniqueValidator = require('mongoose-unique-validator');
 
 const Schema = mongoose.Schema;
@@ -15,6 +16,19 @@ const userSchema = new Schema({
   bio: { type: String, required: true },
   socials: [{type: String, required: true}],
   apps: [{ type: mongoose.Types.ObjectId, required: true, ref: "App" }],
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 userSchema.plugin(uniqueValidator);
